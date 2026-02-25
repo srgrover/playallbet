19c69a401a499d610a5d438d6c3bdf8d3c7339b8
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2026-02-25 09:43:08 +0000

    Added new font, background image and material icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <header>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </header>
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
