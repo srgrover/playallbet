630806fcff062bb2bbc169c94e95b9d3f87a836b
dev.jonathan.moya <dev.jonathan.moya@gmail.com>
2026-03-03 11:31:19 +0000

    New event interface and login form with provider ok today

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,7 +7,7 @@ import { LoginForm } from "./ui/LoginForm";
 export default function Login() {
     const { data: session } = useSession();
 
-    if(session) redirect('/dashboard')
+    if(session) redirect('/home')
     return (
         <div className="flex min-w-screen flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <LoginForm />
